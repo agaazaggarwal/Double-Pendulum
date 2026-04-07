65d3fcfa4910181aeb8446c2ce4819b2a2bf3cfb
Agaaz Aggarwal <88220022+agaazaggarwal@users.noreply.github.com>
2026-04-07 23:25:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+VIDEO_PATH = "pendulum.mp4"      
+L1 = 0.28                         
+L2 = 0.21                         
+M1 = 0.04                         
+M2 = 0.08                        
+G = 9.81                         
+
+
+SMOOTHING_WINDOW = 5
+PERTURBATION_DEG = 1.0
